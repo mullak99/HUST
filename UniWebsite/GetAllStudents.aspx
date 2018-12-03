7765a4f854795d01a5b5775ea3949ade92d7cf5e
mullak99 <mullak99@mullak99.co.uk>
2018-12-03 20:35:02 +0000

    Added page to get all data from database

---
+++ UniWebsite/GetAllStudents.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GetAllStudents.aspx.cs" Inherits="UniWebsite.GetAllStudents" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>RDE Server | All Students</title>
+</head>
+<body style="background-color:#282828">
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="allStudentsTable" runat="server" ForeColor="White"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ UniWebsite/GetAllStudents.aspx.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UniWebsite
+{
+    public partial class GetAllStudents : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (var connection = new MySqlConnection(WebConfigurationManager.ConnectionStrings["sqlDbConnectionString"].ConnectionString))
+            {
+                var query = "SELECT LastName, FirstName, Location, Time FROM students";
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    connection.Open();
+                    MySqlDataReader dr = command.ExecuteReader(CommandBehavior.CloseConnection);
+                    DataTable dt = new DataTable();
+                    dt.Load(dr);
+                    allStudentsTable.DataSource = dt;
+                    allStudentsTable.DataBind();
+                }
+            }
+        }
+    }
+}
